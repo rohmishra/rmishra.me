022083782065b77d65315bd6a86904d62ef46e07
Rohan M <rohan@crazydeveloper.fail>
2022-04-10 21:21:00 -0400

    test: Describe tests (nonfunctional)
    Signed-off-by: Rohan M <rohan@crazydeveloper.fail>

--- test/homepage.test.js
+++ test/homepage.test.js
@@ -1,3 +1,23 @@
-test('example test, should always pass', () => {
-	expect(2 + 2 == 4);
+describe("Checking if website launches as expected", () => {
+  test("Sanity check, should always pass", () => {
+    expect(2 + 2 == 4);
+  });
+
+  test("website has navigation", () => {
+    expect(true); //TODO: Check if nav element exists
+  });
+
+  test("Check hero element", () => {
+    expect(true); //TODO: Check if H1 exists
+  });
+
+  test("website has footer", () => {
+    expect(true); //TODO: Check if footer element exists
+  });
+});
+
+describe("Send example mail", () => {
+  test("Send email", () => {
+    expect(true); // Send mail by emulating a user
+  });
 });
